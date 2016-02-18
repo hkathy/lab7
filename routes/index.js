2eeb0b8b5b018e33a88e0df41f5cc572f1869c23
Kathy Huynh <huynh.k100@gmail.com>
2016-02-18 20:18:07 +0000

    added experiment

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,20 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
-exports.view = function(req, res){
-  	res.render('index', projects);
-  	projects["grid"] = false;
-};
+ exports.view = function(req, res){
+ 	var random_num = Math.random();
+ 	console.log(random_num);
+
+ 	if (random_num >= 0) {
+ 		projects['grid'] = false;
+ 		res.render('index', projects);
+ 	} else {
+ 		res.redirect('/grid');
+ 	}
+ };
 
 exports.viewGrid = function(req, res){
-  	res.render('index', projects);
-  	projects["grid"] = true;
+	projects["grid"] = true;
+	res.render('index', projects);
 };
 
